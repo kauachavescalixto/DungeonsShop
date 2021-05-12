0fd6d3b0480fc4a87da9a993d5aaed35d63a6e65
kauachavescalixto <80490049+kauachavescalixto@users.noreply.github.com>
2021-05-11 23:56:13 -0300

    Add files via upload
    adicionadas novas imagens e funções de botões + barras de filtro com botões funcionando mas sem registro, ainda

---
+++ Dungeon_Shop/src/aloha/dungeons/testes/BarPanel.java
@@ -0,0 +1,72 @@
+package aloha.dungeons.testes;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class BarPanel extends JPanel{
+
+	JLabel on = new JLabel(new ImageIcon(getClass().getResource("Barra499X37.png")));
+	JLabel off = new JLabel(new ImageIcon(getClass().getResource("BarraOFF499X37.png")));
+	
+	JCheckBox check = new JCheckBox();
+	
+	JLabel txt;
+	
+	public BarPanel(String condition) {
+		init();
+		def_eventos();
+		txt = new JLabel(condition);
+		
+		off.setSize(499,37);
+		on.setSize(499,37);
+		check.setSize(50,29);
+		check.setLocation(400,5);
+		check.setContentAreaFilled(false);
+
+		txt.setSize(400,37);
+		txt.setLocation(10,0);
+		txt.setForeground(Color.white);
+		txt.setFont(new Font("Courier New", Font.BOLD,18));
+		
+		
+		check.setOpaque(false);
+		
+		
+		add(txt);
+		
+		add(on);
+		on.setVisible(false);
+		add(off);
+		add(check);
+		
+	}
+
+	public void init() {
+		setOpaque(false);
+		setLayout(null);
+		setSize(499,37);
+		setBackground(new Color(0,0,0,0));
+	}
+	
+	public void def_eventos() {
+		check.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(check.isSelected()) {
+					on.setVisible(true);
+					off.setVisible(false);
+				}else {
+					off.setVisible(true);
+					on.setVisible(false);
+				}
+			}
+		});
+	}
+}
---
+++ Dungeon_Shop/src/aloha/dungeons/testes/ItemPanel.java
@@ -0,0 +1,116 @@
+package aloha.dungeons.testes;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import javafx.scene.image.Image;
+
+public class ItemPanel extends JPanel{
+	JPanel pnEme = new JPanel();
+	JPanel pnImg = new JPanel();
+	JPanel pnall;
+	JLayeredPane lpn = new JLayeredPane();
+	
+	JLabel emerald = new JLabel(new ImageIcon(getClass().getResource("Emerald17X24.png")));
+	ImageIcon frameIcon = new ImageIcon(getClass().getResource("Frame.png"));
+	JLabel frame = new JLabel(frameIcon);
+	JLabel item, nome ,preco;
+	int[] precos = new int[10];
+	
+	JCheckBox ck = new JCheckBox();
+
+	String[] itens = new String[] {
+			"Guarda","Incendiária","Luvas","Mago","MagoDaSelva",
+			"Mercenario","Ocelote","Picareta","PresasDoGelo","Tempestuoso"
+	};
+	ImageIcon btoff = new ImageIcon(getClass().getResource("buyButton20X21.png"));
+	ImageIcon bton = new ImageIcon(getClass().getResource("buyButtonON20X21.png"));
+	
+	ArrayList<Integer> selecionados = new ArrayList<>();
+	
+	
+	public ItemPanel(int index) {
+		
+		for (int i = 0; i < precos.length; i++) {
+			precos[i]=(int) (Math.random()*500);
+		}
+		init();
+		def(index);
+		
+		item = new JLabel(new ImageIcon(getClass().getResource(itens[index]+".png")));
+		preco = new JLabel(String.valueOf(precos[index]));
+		nome = new JLabel(itens[index],SwingConstants.CENTER);
+		pnEme.add(emerald);
+		pnEme.add(preco);
+		pnImg.setLayout(new GridLayout(0,1));
+		pnImg.add(item);
+		preco.setForeground(Color.white);
+		nome.setForeground(Color.white);
+		preco.setFont(new Font("Courier new", 0,15));
+		nome.setFont(new Font("Courier new", 0,15));
+		pnEme.setBackground(null);
+		pnImg.setBackground(null);
+		
+		ck.setSize(120,180);
+		ck.setOpaque(false);
+		ck.setIcon(btoff);
+		
+		
+		pnall = new JPanel(new BorderLayout());
+		pnall.setSize(111,180);
+		pnall.setBackground(new Color(0,0,0,0));
+		
+		pnall.add(pnEme, "North");
+		pnall.add(pnImg, "Center");
+		pnall.add(nome, "South");
+		pnall.setOpaque(false);
+		
+		lpn.add(pnall);
+		lpn.add(ck);
+		lpn.setPreferredSize(new Dimension(111,180));
+		lpn.setOpaque(false);
+		add(lpn);
+		
+		
+		
+	}
+	public void def(int index) {
+		ck.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(ck.isSelected()) {
+					ck.setIcon(bton);
+					MineDungeonsShop.selected.add(index);
+					
+				}else {
+					ck.setIcon(btoff);
+					for (int i = 0; i < MineDungeonsShop.selected.size(); i++) {
+						if(MineDungeonsShop.selected.get(i)==index) {
+							MineDungeonsShop.selected.remove(i);
+						}
+					}
+					
+				}
+			}
+		});
+	}
+
+	public void init() {
+		setOpaque(false);
+		setSize(111,180);
+		setLayout(new BorderLayout(0,0));
+		setBorder(BorderFactory.createLineBorder(Color.white,3));
+		setBackground(new Color(13,8,13));
+	}
+
+}
